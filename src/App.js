eabeea3ffe2571f2bcb8b5ce096786c805684b02
Rudy Olsson <rudyolsson@gmail.com>
2018-09-27 16:44:38 +0200

    added routes, store, app state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header/Header';
+import classes from './App.css';
+
 class App extends Component {
   render() {
-    return <div>Hello!</div>;
+    return (
+      <div>
+        <Header />
+        <main className={classes.Content}>{this.props.children}</main>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Root from './root';
-import App from './app';
+import App from './App';
+import Home from './components/Home/Home';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
+import SignOut from './components/SignOut/SignOut';
+import Invoices from './components/Invoices/Invoices';
+import './index.css';
 
 ReactDOM.render(
   <Root>
     <BrowserRouter>
-      <Route path="/" component={App} />
+      <App>
+        <Route path="/" component={Home} exact />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signout" component={SignOut} />
+        <Route path="/invoices" component={Invoices} />
+      </App>
     </BrowserRouter>
   </Root>,
   document.querySelector('#root')
