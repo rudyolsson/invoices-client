408deb300e4d9fd804746638c1d93a712db13665
Rudy Olsson <rudyolsson@gmail.com>
2018-10-01 14:50:51 +0200

    removed redux dev tools for deployment

--- src/root.js
+++ src/root.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './store/reducers';
-
-const composeEnhancers =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./store/reducers";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(
-    reducers,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk))
-  );
+  const store = createStore(reducers, initialState, applyMiddleware(thunk));
   return <Provider store={store}>{children}</Provider>;
 };
